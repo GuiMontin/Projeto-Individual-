258c213994dd5420613565bffca9e3ffaf44db0f
GuiMontin <guilherme.alves@sptech.school>
2024-11-12 16:22:33 -0300

    Atualizando site

---
+++ site index/script.js
@@ -0,0 +1,6 @@
+let contador = 0;
+setInterval(() => {
+    const sliderContainer = document.querySelector('.slider-containner');
+    contador = (contador + 1) % 3; // alterna entre 0, 1 , 2 e 3 (total de 4 ou a sua escolha rs rs imagens)
+    sliderContainer.style.transform = `translateX(-${contador * 100}vw)`;
+}, 3000); // Alterne as imagens a cada 5 segundos
